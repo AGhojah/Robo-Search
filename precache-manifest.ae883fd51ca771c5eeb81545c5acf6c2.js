edb19459e89a91738d0c58160eb6c3185cf5306e
AGhojah <ghokah@gmail.com>
2019-03-17 23:22:07 +0200

    Updates

---
+++ precache-manifest.ae883fd51ca771c5eeb81545c5acf6c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7731d890a64cf98f9e61",
+    "url": "/Robo-Search/static/js/runtime~main.44719d61.js"
+  },
+  {
+    "revision": "1c878dd294f492b94661",
+    "url": "/Robo-Search/static/js/main.993879f3.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/Robo-Search/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "1c878dd294f492b94661",
+    "url": "/Robo-Search/static/css/main.a9d6b97a.chunk.css"
+  },
+  {
+    "revision": "56d3214203d327ec5aabb4b60941fd14",
+    "url": "/Robo-Search/index.html"
+  }
+];
