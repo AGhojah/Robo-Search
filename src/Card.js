37877980d0ca7a0df9401f5fd29ea8f05284fb35
AGhojah <ghokah@gmail.com>
2019-03-17 19:40:26 +0200

    Adding First Component, Card

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+const Card = ({name, email, id}) => {
+    return (
+        <div className='basic'>
+            <img alt='Pic' src={`https://robohash.org/${id}?size=200x200`} />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div>
+                    <Card name={"Mike"} email={"Mike@gmail.com"} id={12}/>
+                    <Card name={"Ralph"} email={"Ralph@gmail.com"} id={22}/>
+                    <Card name={"Boris"} email={"Boris@gmail.com"} id={15}/>
+                    <Card name={"Sa'ed"} email={"Sa'ed@gmail.com"} id={42}/>
+                </div> 
+    
+    
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
